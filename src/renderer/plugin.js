5de99aed2b1f7849fb616d3c60a30169f8d788ef
zhixu <zhixu93@outlook.com>
2016-09-09 22:05:58 -0400

    successfully three pics

--- src/renderer/plugin.js
+++ src/renderer/plugin.js
@@ -1,34 +1,38 @@
+function myOnLoad() {
+	CKEDITOR.plugins.add( 'emoji',{
+		init: function( editor )
+		{
+			editor.addCommand( 'popUpDialog', {
+				exec : function( editor ){    
+					 $("#myModal").modal() 
+				}
+			});
+			editor.ui.addButton( 'btnInsert',{
+				label: 'Insert a Link',
+				command: 'popUpDialog',
+				icon: this.path + 'images/icon.png'
+			});
+		}
+	} );
 
-CKEDITOR.plugins.add( 'emoji',{
-	init: function( editor )
-	{
-		editor.addCommand( 'popUpDialog', {
-			exec : function( editor ){    
-				 $("#myModal").modal() 
-			}
-		});
-		editor.ui.addButton( 'btnInsert',{
-			label: 'Insert a Link',
-			command: 'popUpDialog',
-			icon: this.path + 'images/icon.png'
-		});
-	}
-} );
+	CKEDITOR.replace( 'editor1',{
+		extraPlugins : 'emoji',
+		toolbar :
+		[
+			['Source', '-', 'Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink'],
+			['About','-','btnInsert']
+		]
+	});
 
-CKEDITOR.replace( 'editor1',{
-	extraPlugins : 'emoji',
-	toolbar :
-	[
-		['Source', '-', 'Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink'],
-		['About','-','btnInsert']
-	]
-});
+	var canvas = document.getElementById("canvas");
+	var meme = new glTFEmojiRenderer.Meme(canvas, "/glTFs/CesiumMan.gltf");
+	meme.createEmoji();
 
-var canvas = document.getElementById("canvas");
-glTFEmojiRenderer.createEmoji(canvas, "/glTFs/CesiumMan.gltf");
+	var canvas2 = document.getElementById("canvas2");
+	var meme2 = new glTFEmojiRenderer.Meme(canvas2, "/glTFs/CesiumMilkTruck.gltf");
+	meme2.createEmoji();
 
-// var canvas2 = document.getElementById("canvas2");
-// glTFEmojiRenderer.createEmoji(canvas2, "/glTFs/CesiumMilkTruck/glTF/CesiumMilkTruck.gltf");
-
-// var canvas3 = document.getElementById("canvas3");
-// glTFEmojiRenderer.createEmoji(canvas3, "/glTFs/duck/glTF/duck.gltf");
+	var canvas3 = document.getElementById("canvas3");
+	var meme3 = new glTFEmojiRenderer.Meme(canvas3, "/glTFs/duck/glTF/duck.gltf");
+	meme3.createEmoji();
+}

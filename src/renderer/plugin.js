9e8aacab1a844ac9786206c21cfc1405d485a1d5
zhixu <zhixu93@outlook.com>
2016-09-10 14:58:56 -0400

    require zhanweifu

--- src/renderer/plugin.js
+++ src/renderer/plugin.js
@@ -24,8 +24,8 @@ function myOnLoad() {
 		]
 	});
 
-	var canvas = document.getElementById("canvas1");
-	var meme = new glTFEmojiRenderer.Meme(canvas, "/glTFs/CesiumMan.gltf");
+	var canvas1 = document.getElementById("canvas1");
+	var meme = new glTFEmojiRenderer.Meme(canvas1, "/glTFs/CesiumMan.gltf");
 	meme.createEmoji();
 
 	var canvas2 = document.getElementById("canvas2");
@@ -35,4 +35,54 @@ function myOnLoad() {
 	var canvas3 = document.getElementById("canvas3");
 	var meme3 = new glTFEmojiRenderer.Meme(canvas3, "/glTFs/duck/glTF-MaterialsCommon/duck.gltf");
 	meme3.createEmoji();
+}
+
+function okClick(){
+	canvasIds = [];
+	var conte
+	for(var cb in checkboxCanvasMap){
+		if(document.getElementById(cb).checked){
+			canvasIds.push(checkboxCanvasMap[cb]);
+		}
+	}
+	// alert(document.getElementById('cb1').checked);
+}
+
+var canvasIds = [];
+var displayDivIdsNum = 0;
+var displayCanvasIdsNum = 0;
+var displayCanvasCanvasMap = {};
+//mapping gltf files to canvas
+var map = [];
+map["canvas1"] = "/glTFs/CesiumMan.gltf";
+map["canvas2"] = "/glTFs/CesiumMilkTruck.gltf";
+map["canvas3"] = "/glTFs/duck/glTF-MaterialsCommon/duck.gltf";
+
+var checkboxCanvasMap = {
+	cb1:"canvas1",
+	cb2:"canvas2",
+	cb3:"canvas3"
+};
+
+function displayAll(){
+	var content = CKEDITOR.instances.editor1.getData();
+	var curDispCanvasIds = [];
+	for(cid in canvasIds){
+		var displayCanvasId = "displayCanvas"+displayCanvasIdsNum++;
+		displayCanvasCanvasMap[displayCanvasId] = canvasIds[cid];
+		content += '<canvas id="' + displayCanvasId + '" class="canvases" width="180px"></canvas>';
+		curDispCanvasIds.push(displayCanvasId);
+	}
+	var newDiv = document.createElement("div");
+	var newId = "displayDiv"+displayDivIdsNum++;
+	newDiv.id = newId;
+	newDiv.innerHTML = content;
+	newDiv.className = "well";
+
+	document.getElementById("display").appendChild(newDiv);
+	for(cid in curDispCanvasIds){
+		var canvas = document.getElementById(curDispCanvasIds[cid]);
+		var meme = new glTFEmojiRenderer.Meme(canvas, map[displayCanvasCanvasMap[curDispCanvasIds[cid]]]);
+		meme.createEmoji();
+	}
 }

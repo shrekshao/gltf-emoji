c2597026a3c039b0c795a352943d09951d69994c
Windy Darian <p123456638@msn.com>
2016-09-10 23:47:05 -0400

    JackieChan2

---
+++ glTFs/JackieChan2/wtf24_30VS.glsl
@@ -0,0 +1,30 @@
+precision highp float;
+attribute vec3 a_position;
+attribute vec3 a_normal;
+varying vec3 v_normal;
+attribute vec4 a_joint;
+attribute vec4 a_weight;
+uniform mat4 u_jointMat[3];
+uniform mat3 u_normalMatrix;
+uniform mat4 u_modelViewMatrix;
+uniform mat4 u_projectionMatrix;
+attribute vec2 a_texcoord0;
+varying vec2 v_texcoord0;
+varying vec3 v_light0Direction;
+varying vec3 v_position;
+uniform mat4 u_light0Transform;
+varying vec3 v_light1Direction;
+uniform mat4 u_light1Transform;
+void main(void) {
+mat4 skinMat = a_weight.x * u_jointMat[int(a_joint.x)];
+skinMat += a_weight.y * u_jointMat[int(a_joint.y)];
+skinMat += a_weight.z * u_jointMat[int(a_joint.z)];
+skinMat += a_weight.w * u_jointMat[int(a_joint.w)];
+vec4 pos = u_modelViewMatrix * skinMat * vec4(a_position,1.0);
+v_normal = u_normalMatrix * mat3(skinMat)* a_normal;
+v_texcoord0 = a_texcoord0;
+v_position = pos.xyz;
+v_light0Direction = u_light0Transform[3].xyz - pos.xyz;
+v_light1Direction = u_light1Transform[3].xyz - pos.xyz;
+gl_Position = u_projectionMatrix * pos;
+}
